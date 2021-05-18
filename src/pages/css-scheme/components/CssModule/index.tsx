5d690a83a5008f3d1c18661be7f17fea6f28a7a6
GaryHjy <782316959@qq.com>
2021-05-18 17:07:33 +0800

    feat: 更新demo

--- src/pages/css-scheme/components/CssModule/index.tsx
+++ src/pages/css-scheme/components/CssModule/index.tsx
@@ -1,7 +1,9 @@
+import styles from './index.less';
+
 const CssModule = () => {
   return (
-    <div>
-      <h2>CSS Modules</h2>
+    <div className={styles['css-module']}>
+      <h2 className={styles.title}>CSS Modules</h2>
     </div>
   );
 };
