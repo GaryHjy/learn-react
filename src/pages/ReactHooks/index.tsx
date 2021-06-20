ef19fce773275a51be3a76276c25c114131842e7
GaryHjy <782316959@qq.com>
2021-06-20 23:18:24 +0800

    feat: 新增hooks文件夹

--- src/pages/ReactHooks/index.tsx
+++ src/pages/ReactHooks/index.tsx
@@ -1,5 +1,50 @@
-const ReactHooks = () => {
-  return <div>React Hooks</div>;
+import { FC, useCallback, useMemo } from 'react';
+import { history, useLocation } from 'umi';
+
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
+
+const TAB_CONFIG = [
+  {
+    title: 'useState',
+    key: '/react-hooks/use-state',
+  },
+  {
+    title: 'useEffect',
+    key: '/react-hooks/use-effect',
+  },
+  {
+    title: 'useContext',
+    key: '/react-hooks/use-context',
+  },
+];
+
+const ReactHooks: FC = (props) => {
+  const { pathname } = useLocation();
+
+  const path = useMemo(() => pathname, [pathname]);
+
+  // tab切换
+  const onChangeTabs = useCallback(
+    (key: string) => {
+      if (key !== path) {
+        history.push(key);
+      }
+    },
+    [path],
+  );
+
+  return (
+    <div>
+      <Tabs onChange={onChangeTabs} activeKey={path}>
+        {TAB_CONFIG.map((tab) => (
+          <TabPane tab={tab.title} key={tab.key} />
+        ))}
+      </Tabs>
+      {props.children}
+    </div>
+  );
 };
 
 export default ReactHooks;
