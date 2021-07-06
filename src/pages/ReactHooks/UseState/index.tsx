d42e83b51bbc42afed2f33400f7050643d270dcd
GaryHjy <782316959@qq.com>
2021-07-06 21:22:20 +0800

    feat: 添加useState例子

--- src/pages/ReactHooks/UseState/index.tsx
+++ src/pages/ReactHooks/UseState/index.tsx
@@ -1,5 +1,40 @@
+import { useState, Component } from 'react';
+
+class Example extends Component {
+  state = {
+    count: 0,
+  };
+  render() {
+    return (
+      <>
+        <div>{this.state.count}</div>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          changeCount
+        </button>
+      </>
+    );
+  }
+}
+
+const Example1 = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>changeCount</button>
+    </>
+  );
+};
+
 const UseState = () => {
-  return <div>UseState</div>;
+  return (
+    <div>
+      <Example />
+      <hr />
+      <Example1 />
+    </div>
+  );
 };
 
 export default UseState;
