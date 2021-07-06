24f3380f20a2c8f31b070a3da26513229d3da292
GaryHjy <782316959@qq.com>
2021-07-06 20:55:49 +0800

    feat: 添加webpack5及splitChunks

--- .umirc.ts
+++ .umirc.ts
@@ -15,4 +15,44 @@ export default defineConfig({
   },
   routes,
   fastRefresh: {},
+  webpack5: {},
+  chunks: ['reactVendor', 'antd', 'umiVendor', 'vendors', 'umi'],
+  chainWebpack(config) {
+    config.merge({
+      optimization: {
+        splitChunks: {
+          chunks: 'all',
+          minSize: 30000,
+          minChunks: 3,
+          automaticNameDelimiter: '.',
+          cacheGroups: {
+            reactVendor: {
+              test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+              name: 'reactVendor',
+              enforce: true,
+              priority: 4,
+            },
+            antd: {
+              test: /[\\/]node_modules[\\/](antd|@ant-design)[\\/]/,
+              name: 'antd',
+              enforce: true,
+              priority: 3,
+            },
+            umiVendor: {
+              test: /[\\/]node_modules[\\/](umi).*[\\/]/,
+              name: 'umiVendor',
+              enforce: true,
+              priority: 2,
+            },
+            vendors: {
+              name: 'vendors',
+              enforce: true,
+              priority: 1,
+              test: /[\\/]node_modules[\\/]((?!(antd|@ant-design|react|react-dom|umi)).*)[\\/]/,
+            },
+          },
+        },
+      },
+    });
+  },
 });
