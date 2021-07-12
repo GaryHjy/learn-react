353939cb52dccf6d5b05009f106fcc7261665ecd
GaryHjy <782316959@qq.com>
2021-07-12 23:51:21 +0800

    feat: 添加说明

--- src/pages/ReactHooks/UseState/index.tsx
+++ src/pages/ReactHooks/UseState/index.tsx
@@ -1,5 +1,12 @@
 import { useState, Component } from 'react';
 
+/**
+ * 类组件与函数式组件写法的区别
+ * 1. 类组件定义state是直接在this.state中定义
+ * 2. 函数式组件的state定义是通过useState hook来定义
+ */
+
+// 类组件
 class Example extends Component {
   state = {
     count: 0,
@@ -16,6 +23,7 @@ class Example extends Component {
   }
 }
 
+// 函数式组件
 const Example1 = () => {
   const [count, setCount] = useState(0);
 
